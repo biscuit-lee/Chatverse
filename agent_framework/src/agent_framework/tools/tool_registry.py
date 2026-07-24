18aa72ccefb2a0aead2c572056b5e832aa25e7d5
biscuit-lee <sophearothprum168@gmail.com>
2026-07-24 05:24:32 +0200

    feat: add tool registry class

---
+++ agent_framework/src/agent_framework/tools/tool_registry.py
@@ -0,0 +1,21 @@
+from agent_framework.tools.base_tool import BaseTool
+class ToolRegistry:
+    """
+    Tool registry class 
+    
+    """
+
+    def __init__(self):
+        self.tools: dict[str,BaseTool] = {}
+
+    def register(self,tool: BaseTool):
+        if tool.name in self.tools:
+            raise ValueError(f"Tool with name '{tool.name}' is already registered.")
+
+        self.tools[tool.name] = tool
+
+    def get(self,tool_name: str) -> BaseTool:
+        return self.tools.get(tool_name)
+
+    def all(self) -> list[BaseTool]:
+        return list(self.tools.values())
