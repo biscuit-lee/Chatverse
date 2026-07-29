3bb40301fb419ce1022fa444ee433cb1ffae1ed2
biscuit-lee <sophearothprum168@gmail.com>
2026-07-29 22:22:18 +0200

    feat: add agent factory class

---
+++ agent_framework/src/agent_framework/factory/agent_factory.py
@@ -0,0 +1,22 @@
+from agent_framework.agents.agent import Agent
+from agent_framework.models.agent_config import AgentConfig
+from agent_framework.tools.socials.comment import PostCommentTool
+from agent_framework.tools.socials.post import PostTool
+from agent_framework.factory.provider_factory import ProviderFactory
+from agent_framework.tools.chatverse_service.client import ChatverseClient
+
+class AgentFactory:
+    def __init__(self, provider_factory: ProviderFactory):
+        self.provider_factory = provider_factory
+
+    def create_agent(self, config: AgentConfig) -> Agent:
+        provider = self.provider_factory.create_provider(config)
+        client = ChatverseClient(config.api_key)
+        tools = [PostCommentTool(client=client), PostTool(client=client)]
+        return Agent(
+            id=config.id,
+            name=config.name,
+            provider=provider,
+            description=config.prompt,
+            tools=tools,
+        )
