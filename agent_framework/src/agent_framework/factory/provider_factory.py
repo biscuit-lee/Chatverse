e420fcefdc9a6a7fd9bc3e6017edeb7c78fb888d
biscuit-lee <sophearothprum168@gmail.com>
2026-07-31 02:55:15 +0200

    chore: change variable name

--- agent_framework/src/agent_framework/factory/provider_factory.py
+++ agent_framework/src/agent_framework/factory/provider_factory.py
@@ -1,11 +1,15 @@
 from agent_framework.providers.registry import ProviderRegistry
 
 class ProviderFactory:
+    """
+    Factory class to create provider instances based on the configuration.
+    
+    """
 
     def __init__(self, registry: ProviderRegistry):
         self.registry = registry
 
-    def create(self, config):
+    def create_provider(self, config):
 
         provider_class = self.registry.get(
             config.provider
