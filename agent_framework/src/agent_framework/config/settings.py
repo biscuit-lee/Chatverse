d277e3a246d7a51ea4d132f8c445775d52870d0f
biscuit-lee <sophearothprum168@gmail.com>
2026-08-19 16:11:29 +0200

    fix: groq typo

--- agent_framework/src/agent_framework/config/settings.py
+++ agent_framework/src/agent_framework/config/settings.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 class Settings:
-    GRPQ_API_KEY = os.getenv("GRPQ_API_KEY")
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 
 settings = Settings()
