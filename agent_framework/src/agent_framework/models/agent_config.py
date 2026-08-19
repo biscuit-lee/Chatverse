ecb00d9c31859941b07b01b13cf73aa65b3bd773
biscuit-lee <sophearothprum168@gmail.com>
2026-08-19 17:04:06 +0200

    fix: use int for id in agentconfig

--- agent_framework/src/agent_framework/models/agent_config.py
+++ agent_framework/src/agent_framework/models/agent_config.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class AgentConfig:
-    id: str
+    id: int
     name: str
     model: str
     prompt: str
--- agent_framework/src/agent_framework/runtime/agent_loop.py
+++ agent_framework/src/agent_framework/runtime/agent_loop.py
@@ -1,8 +1,6 @@
-from agent_framework.agents.agent import Agent
-from agent_framework.providers.groq_provider import GroqProvider
-from agent_framework.tools.socials.post import PostTool
 import json
-from agent_framework.tools.chatverse_service.client import client
+from agent_framework.agents.agent import Agent
+
 
 def run(agent: Agent, user_input: str):
 
@@ -11,34 +9,28 @@ def run(agent: Agent, user_input: str):
             "role": "system",
             "content": agent.description
         },
-
         {
             "role": "user",
             "content": user_input
         }
-
     ]
 
     result = agent.provider.generate(messages=messages, tools=agent.tool_registry.all())
 
     while True:
-        # Agent wants to do tool call
         if result.tool_calls:
 
-            # Add agent's tool request to history
             messages.append({
-                "role" :"assistant",
-                "content" : result.content,   # None
-                "tool_call":result.tool_calls   
+                "role": "assistant",
+                "content": result.content,
+                "tool_calls": result.tool_calls
             })
-
 
             for call in result.tool_calls:
                 tool_name = call.function.name
                 tool_arg = json.loads(call.function.arguments)
 
                 tool = agent.tool_registry.get(tool_name=tool_name)
-
                 tool_result = tool.execute(tool_arg)
 
                 messages.append({
@@ -49,13 +41,4 @@ def run(agent: Agent, user_input: str):
                 print(tool_result)
         else:
             print(result.content)
-
             return result.content
-
-
-    return result
-
-if __name__ == "__main__":
-    agent = Agent(name="TestAgent", description="You are an assistant that talks in riddles", tools=[PostTool()], provider=GroqProvider(model="openai/gpt-oss-120b"))
-    run(agent, user_input="Create a post saying 'Hello from my agent'")
-
